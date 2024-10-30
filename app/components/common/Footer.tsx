a83e374325b2d633018e1cdff26535a9ee61df3c
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-10-30 21:11:06 +0900

    feat: footer_in_main_page

---
+++ app/components/common/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="text-left p-5 bg-white text-gray-500 text-sm">
+      <div className="border-t border-gray-300 w-full mb-3 flex font-light"></div>
+      <p>© 2024 urbansgold all reserved</p>
+      <p>
+        (주)어반스골드 | 주소 : 경기도 성남시 중원구 갈마치로 215, 지3층 301호 | 사업자등록번호 : 237-81-01497 | 
+      </p>
+    </div>
+  )
+}
