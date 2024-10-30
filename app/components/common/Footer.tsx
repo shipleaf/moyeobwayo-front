e95836c2bc25dfc8b92c75a372aafabd02435d6b
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-10-30 21:58:06 +0900

    feat: privacyPolicy

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -4,9 +4,11 @@ export default function Footer() {
   return (
     <div className="text-left p-5 bg-white text-gray-500 text-sm">
       <div className="border-t border-gray-300 w-full mb-3 flex font-light"></div>
-      <p>© 2024 urbansgold all reserved</p>
+      <p>© 2024 urbansgold all reserved |  
+        <a href="/privacyPolicy" className="text-inherit"> 개인정보 처리 방침</a>
+      </p>
       <p>
-        (주)어반스골드 | 주소 : 경기도 성남시 중원구 갈마치로 215, 지3층 301호 | 사업자등록번호 : 237-81-01497 | 
+        (주)어반스골드 | 주소 : 경기도 성남시 중원구 갈마치로 215, 지3층 301호 | 사업자등록번호 : 237-81-01497 | 이메일 : dla9319@korea.ac.kr
       </p>
     </div>
   )
