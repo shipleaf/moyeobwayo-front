3883225be2e4fb06cfee6e221c559cd6be7e8bf9
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-11-06 22:19:39 +0900

    feat: version_up_privacy_policy

--- app/privacyPolicy/page.tsx
+++ app/privacyPolicy/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const page: React.FC = () => {
-  const pdfUrl = 'docs/[모여봐요]개인정보처리방침.pdf';
+  const pdfUrl = 'docs/모여봐요개인정보처리방침_v2.pdf';
 
   return (
     <div className="h-screen flex justify-center items-center bg-gray-50">
