96b25c47eddc826244186c0535b688ab457e48b2
shipleaf <162288294+shipleaf@users.noreply.github.com>
2024-11-06 22:11:37 +0900

    1106

--- app/api/axiosInstance.ts
+++ app/api/axiosInstance.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  "https://gyural.shop:8080/api/v1";
+const API_URL = process.env.NEXT_PUBLIC_SEVER_URL;
 // const API_URL = "http://127.0.0.1:8080/api/v1";
 
 const axiosInstance = axios.create({
