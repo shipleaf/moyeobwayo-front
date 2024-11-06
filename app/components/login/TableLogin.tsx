cc618e1af700797b7b3cbabec9b06e3366b2076e
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-11-07 02:13:06 +0900

    fix: conflict

--- app/components/login/TableLogin.tsx
+++ app/components/login/TableLogin.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+'use client'
+
+import React, { useEffect, useState } from "react";
 import { useSetRecoilState, useRecoilValue, useRecoilState } from "recoil";
 import { loginState, userIdValue, kakaoUserState } from "@/app/recoil/atom"; // Recoil 상태
 import { LoginData } from "@/app/api/tableLogin";
@@ -6,44 +8,66 @@ import { useParams } from "next/navigation"; // useParams를 import
 import { linkKakaoAndPartyUser } from "@/app/api/kakaoLoginAPI";
 import { tableLoginHandler } from "@/app/utils/tableLoginCallback";
 import Image from "next/image";
+import Script from "next/script";
+import { kakaoLoginHandlerbyMeeting } from "@/app/meeting/service/kakaoLoginHandler";
 
 export default function TableLogin() {
   const { hash } = useParams() as { hash: string }; // hash를 string으로 단언
   const [userName, setUserName] = useState(""); // 사용자 이름 입력 상태
   const [password, setPassword] = useState(""); // 비밀번호 입력 상태
+  const [isKakaoReady, setIsKakaoReady] = useState(false); // SDK가 준비된 상태를 관리
   const setIsLoggedIn = useSetRecoilState(loginState); // 로그인 상태 관리
   const [userId, setUserId] = useRecoilState(userIdValue);
   const kakaoUser = useRecoilValue(kakaoUserState);
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // LoginData 객체 생성
-    const loginData: LoginData = {
-      userName: userName,
-      password: password,
-      partyId: hash,
-      isKakao: false,
-      kakaoUserId: null
-    };
-
-    try {
-      const response = await tableLoginHandler(loginData, setUserId);
-      console.log("API 응답:", response);
-
-      setIsLoggedIn(true);
-      if(kakaoUser.kakaoUserId !== null){
-        if(userId){
-          await linkKakaoAndPartyUser(userId, kakaoUser.kakaoUserId);
-        }
-      }
-      // 추가적으로 필요한 동작이 있으면 여기에 추가
-    } catch (error) {
-      console.error("로그인 실패:", error);
-    }
+    
+  // LoginData 객체 생성
+  const loginData: LoginData = {
+    userName: userName,
+    password: password,
+    partyId: hash,
+    isKakao: false,
+    kakaoUserId: null
   };
 
+  try {
+    const response = await tableLoginHandler(loginData, setUserId);
+    console.log("API 응답:", response);
+
+    setIsLoggedIn(true);
+    if(kakaoUser.kakaoUserId !== null){
+      if(userId){
+        await linkKakaoAndPartyUser(userId, kakaoUser.kakaoUserId);
+      }
+    }
+    // 추가적으로 필요한 동작이 있으면 여기에 추가
+  } catch (error) {
+    console.error("로그인 실패:", error);
+  }
+  
+  };
+
+  useEffect(() => {
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_KEY as string);
+      console.log("after Init: ", window.Kakao.isInitialized());
+    }
+  }, [isKakaoReady]);
+  
+  const handleKakaoLoginButtonClick = (globalUserID: number | null): void => {
+    setIsKakaoReady(true)
+    kakaoLoginHandlerbyMeeting(globalUserID, hash)
+  }
   return (
     <div className="login-container bg-[#fff] w-full rounded-[10px] shadow-[0px_0px_6px_0px_rgba(0,0,0,0.15)] backdrop-blur-[48px] pt-[15%] p-[10%]">
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
+        onLoad={() => {
+          setIsKakaoReady(true); // SDK 로드 후에만 초기화 진행
+        }}
+      />
       <form
         onSubmit={handleSubmit}
         className="flex flex-col items-center justify-center gap-[2vh] border-[#285cc4]"
@@ -83,6 +107,8 @@ export default function TableLogin() {
             className="p-3  rounded-lg w-4/5 bg-[#FDE500] text-[#3B1D04] hover:bg-[#E5C900] flex items-center justify-center gap-2"
             onClick={() => {
               // Add your Kakao login functionality here
+
+              handleKakaoLoginButtonClick(userId)
             }}
           >
             <Image
---
+++ app/meeting/service/kakaoLoginHandler.ts
@@ -0,0 +1,25 @@
+const redirectUri: string = process.env
+  .NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI as string;
+
+const scope = [
+  "profile_nickname",
+  "profile_image",
+  "talk_message", // 카카오 메시지 동의 항목 추가
+  "talk_calendar",
+].join(",");
+
+export const kakaoLoginHandlerbyMeeting = (globalUserId: number | null,  hash: string) => {
+  if (globalUserId !== null) {
+    // globalUserId를 세션 스토리지에 저장
+    sessionStorage.setItem("globalUserId", globalUserId.toString()); // 문자열로 변환하여 저장
+  }
+  if (window.Kakao && window.Kakao.Auth) {
+    window.Kakao.Auth.authorize({
+      redirectUri,
+      scope,
+    });
+
+    sessionStorage.setItem("have_to_route", hash); // 문자열로 변환하여 저장
+  } else {
+  }
+};
