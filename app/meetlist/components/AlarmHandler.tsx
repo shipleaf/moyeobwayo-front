17c86ca50349e0d7f732f8be496ff164e4ec6144
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-11-08 18:33:42 +0900

    feat: setServerState-파티 알람 on/off

--- app/meetlist/components/AlarmHandler.tsx
+++ app/meetlist/components/AlarmHandler.tsx
@@ -1,19 +1,31 @@
-import { Bell, BellSimpleSlash } from '@phosphor-icons/react/dist/ssr'
-import React from 'react'
-import Toggle from './Toggle'
+import React from 'react';
+import Toggle from './Toggle'; // Toggle 컴포넌트 import 경로는 프로젝트 구조에 맞게 조정하세요.
+import { setServerAlarmState } from '@/app/api/setAlarmState';
+import { Bell, BellSimpleSlash } from '@phosphor-icons/react/dist/ssr';
 
-interface AlarmHandlerProps{
-  alarm: boolean
-  setAlarm: React.Dispatch<React.SetStateAction<boolean>>
+interface AlarmHandlerProps {
+  alarm: boolean;
+  alarmID: number | null;
+  setAlarm: React.Dispatch<React.SetStateAction<boolean>>;
 }
-export default function AlarmHandler({alarm, setAlarm}:AlarmHandlerProps) {
+
+export default function AlarmHandler({ alarm, alarmID, setAlarm }: AlarmHandlerProps) {
+  const handleAlarmToggle = async () => {
+    if (alarmID === null) return;
+
+    const res = await setServerAlarmState(alarmID, !alarm); // 서버 상태 변경 호출
+    console.log('setServerAlarmState Result: ', res);
+
+    setAlarm(prevAlarm => !prevAlarm); // 상태 반전
+  };
+
   return (
     <button className="text-[#5F5F5F] text-[16px] font-semibold py-[9px] px-4
       border-[1px] border-[#D7D7D7] rounded-[50px]
-      flex  items-center gap-4 outline-none">
-        
-      {alarm? (<Bell size={24} />):(<BellSimpleSlash size={24}/>)}
-      <Toggle isToggled={alarm} setAlarm={setAlarm} />
+      flex items-center gap-4 outline-none"
+    >
+      {alarm ? <Bell size={24} /> : <BellSimpleSlash size={24} />}
+      <Toggle isToggled={alarm} handleAlarmToggle={handleAlarmToggle} />
     </button>
-  )
+  );
 }
